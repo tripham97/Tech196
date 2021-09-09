ad9990ecabf00bcb4f4103bb097eabcddecbcdeb
abelpena <73760049+abelpena@users.noreply.github.com>
2021-09-09 15:30:58 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, Text, View, Image, SafeAreaView, ImageBackground } from 'react-native';
+//import {BlurView} from '@react-native-community/blur';
+//import LinearGradient from 'react-native-linear-gradient';
 
+const image = { uri: 'https://www.tech196.com/front_side/images/mainbanner.jpg' };
 //view -> UIView
 export default function App() {
   console.log("App executed");
@@ -9,29 +12,32 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image  style={styles.tinyLogo} source={require("./assets/logo.png")} />
-      <Text numberOfLines={1} onPress={handlePress}>World’s Leading Startup Auction Platform </Text>
-      <StatusBar style="auto" />
-      <Text numberOfLines={1}>Find Your Next Company Here</Text>
-      
-      <View>
-        <Button
-          style={styles.button}
-          onPress={handlePress}
-          title="Login"
-          color="#4b74a8"
-          accessibilityLabel="Login"
-        />
-        <Button
-        style={styles.button}
-        onPress={handlePress}
-        title="Sign Up"
-        color="#4b74a8"
-        accessibilityLabel="Sign Up"
-      />
-      </View>
-
-      
+      <ImageBackground source={require('./assets/splash.png')} style={styles.image}>
+        <Image  style={styles.tinyLogo} source={require("./assets/logo.png")} />
+        <Text numberOfLines={1} onPress={handlePress}>World’s Leading Startup Auction Platform </Text>
+        <StatusBar style="auto" />
+        <Text numberOfLines={1}>Find Your Next Company Here</Text>
+        <View style={styles.glass}>
+        <View style={styles.loginGroup}>
+          <Button
+            style={styles.button}
+            onPress={handlePress}
+            title="Login"
+            color="#4b74a8"
+            accessibilityLabel="Login"
+          />
+        </View>
+          <View style={styles.loginGroup}>
+            <Button
+            style={styles.button}
+            onPress={handlePress}
+            title="Sign Up"
+            color="#4b74a8"
+            accessibilityLabel="Sign Up"
+          />
+        </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -39,7 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: '#A89954',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -50,5 +56,18 @@ const styles = StyleSheet.create({
     paddingVertical: 100,
     flex: 1
     
+  },
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "center",
+    width: 1000,
+    opacity:1,
+  },
+  loginGroup:{
+    paddingTop:50
+  },
+  glass:{
+    opacity:.9
   }
 });
