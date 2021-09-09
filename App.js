78f31ba1838d25b85bec579279a98793c80fad37
abelpena <73760049+abelpena@users.noreply.github.com>
2021-09-09 14:19:30 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground } from 'react-native';
+import { StyleSheet, Button, Text, View, Image, SafeAreaView, ImageBackground } from 'react-native';
 
 //view -> UIView
 export default function App() {
@@ -12,6 +12,26 @@ export default function App() {
       <Image  style={styles.tinyLogo} source={require("./assets/logo.png")} />
       <Text numberOfLines={1} onPress={handlePress}>World’s Leading Startup Auction Platform </Text>
       <StatusBar style="auto" />
+      <Text numberOfLines={1}>Find Your Next Company Here</Text>
+      
+      <View>
+        <Button
+          style={styles.button}
+          onPress={handlePress}
+          title="Login"
+          color="#4b74a8"
+          accessibilityLabel="Login"
+        />
+        <Button
+        style={styles.button}
+        onPress={handlePress}
+        title="Sign Up"
+        color="#4b74a8"
+        accessibilityLabel="Sign Up"
+      />
+      </View>
+
+      
     </View>
   );
 }
@@ -26,4 +46,9 @@ const styles = StyleSheet.create({
   tinyLogo: {
     paddingTop: 50,
   },
+  button: {
+    paddingVertical: 100,
+    flex: 1
+    
+  }
 });
