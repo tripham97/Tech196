0d7cbff632606974d2c986730ed0cc0cb9b94344
abelpena <73760049+abelpena@users.noreply.github.com>
2021-09-13 10:48:35 -0500

    componify and clean up app.js

--- App.js
+++ App.js
@@ -1,42 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, Text, View, Image, SafeAreaView, ImageBackground } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack'
-const Root = createStackNavigator()
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from './components/register.jsx'
+import PreLoginScreen from './components/prelogin.jsx';
+const Stack = createNativeStackNavigator();
+
+function RegisterScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Register Screen</Text>
+      <Text>This is where the details for your registration will be input</Text>
+    </View>
+  );
+}
+
+function LoginDetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Details Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
-  console.log("App executed");
-  const handlePress = () => console.log("Text pressed");
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <ImageBackground source={require('./assets/splash.png')} style={styles.image}>
-        <Image style={styles.tinyLogo} resizeMode={'cover'} source={require("./assets/logo.png")} />
-        <Text style={{fontSize:18}}numberOfLines={1} onPress={handlePress}>World’s Leading Startup Auction Platform </Text>
-        <Text numberOfLines={1} style={styles.subtext}>Find Your Next Company Here</Text>
-
-        <View style={styles.loginGroup}>
-          <Button
-            style={styles.button}
-            onPress={handlePress}
-            title="Login"
-            color="#4b74a8"
-            accessibilityLabel="Login"
-          />
-        </View>
-        <View style={styles.loginGroup}>
-            <Button
-            style={styles.button}
-            onPress={handlePress}
-            title="Sign Up"
-            color="#4b74a8"
-            accessibilityLabel="Sign Up"
-            />
-        </View>
-      
-    </ImageBackground>
-  </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={PreLoginScreen} />
+        <Stack.Screen name="Login Details" component={LoginDetailsScreen} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -45,36 +41,5 @@ const styles = StyleSheet.create({
     flex: 1,
     //backgroundColor: '#A89954',
      alignItems: 'center'
-  },
-  tinyLogo: {
-    flex:0,
-    width:200,
-    paddingTop: 50,
-    marginBottom:50,
-    marginTop:100,
-  },
-  button: {
-    //paddingVertical: 100,
-    marginBottom:30
-    
-    
-  },
-  image: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: "space-between",
-    width: 1000,
-  },
-  loginGroup:{ 
-    margin: 10,
-    paddingTop:0,
-    paddingBottom:0,
-    flex:1,
-    width:300
-  },
-  subtext:{
-    marginBottom:200,
-    marginTop:80,
-    fontSize:20
-  }
+  }  
 });
---
+++ components/prelogin.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+function PreLoginScreen({ navigation }) {
+  console.log("PreLogin Loaded");
+  const handlePress = () => console.log("Text pressed");
+  return (
+    
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <ImageBackground source={require('../assets/splash.png')} style={styles.image}>
+        <Image style={styles.tinyLogo} resizeMode={'cover'} source={require("../assets/logo.png")} />
+        <Text style={{fontSize:18}}numberOfLines={1} onPress={handlePress}>World’s Leading Startup Auction Platform </Text>
+        <Text numberOfLines={1} style={styles.subtext}>Find Your Next Company Here</Text>
+
+        <View style={styles.loginGroup}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => navigation.navigate('Login Details')}
+            
+          >
+            <Text style={{fontSize:20}}>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => navigation.navigate('Register')}
+            ><Text style={{fontSize:20}}>Sign Up</Text>
+          </TouchableOpacity>
+
+        </View>
+        <View style={styles.loginGroup}>
+            
+        </View>
+      
+    </ImageBackground>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#A89954',
+     alignItems: 'center'
+  },
+  tinyLogo: {
+    flex:0,
+    width:200,
+    paddingTop: 50,
+    marginBottom:50,
+    marginTop:100,
+  },
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "space-between",
+    width: 1000,
+  },
+  loginGroup:{ 
+    margin: 10,
+    paddingTop:0,
+    paddingBottom:0,
+    flex:1,
+    width:300
+  },
+  subtext:{
+    marginBottom:200,
+    marginTop:80,
+    fontSize:20
+  },
+  btn:{
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    marginBottom:30
+  }
+});
+
+export default PreLoginScreen;
